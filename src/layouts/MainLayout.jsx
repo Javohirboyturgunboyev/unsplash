c946707df848f8da9c75e7adbb58566a282c3f57
Javohir <javohirboy009@gmail.com>
2025-03-26 17:26:06 +0500

    finished

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
 function MainLayout() {
