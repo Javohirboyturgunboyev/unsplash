85503ce6f38239b97a0d776c662f6678575f7781
Javohir <javohirboy009@gmail.com>
2025-03-26 17:20:49 +0500

    tugadi

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
-import Footer from "../components/footer";
 import Navbar from "../components/Navbar";
+import Footer from "../components/footer";
 import { Outlet } from "react-router-dom";
 
 function MainLayout() {
